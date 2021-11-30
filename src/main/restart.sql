9c2ecdee5c7f7d291e3ce7105bbdbcef949f6ff2
lexfer98 <82776280+lexfer98@users.noreply.github.com>
2021-11-30 13:31:32 +0100

    database

--- src/main/restart.sql
+++ src/main/restart.sql
@@ -1,25 +1,23 @@
-DROP TABLE Usuario;
-
-CREATE TABLE Usuario(
+CREATE TABLE Users(
 	id		VARCHAR(9),
 	balance		INTEGER,
 	card		INTEGER NOT NULL,
 	age		INTEGER NOT NULL CHECK (age > 17),
 	email		VARCHAR(50) NOT NULL,
-	nombre		VARCHAR(50) NOT NULL,
+	name		VARCHAR(50) NOT NULL,
 	password	VARCHAR(50) NOT NULL,
 	phone		INTEGER NOT NULL,
 
-CONSTRAINT cp_user PRIMARY KEY(id)
+CONSTRAINT cp_users PRIMARY KEY(id)
 );
 
 CREATE TABLE Wallet(
     idWallet        VARCHAR(40),
     quantity        INTEGER,
-    idUsuario       VARCHAR(9),
+    idUser          VARCHAR(9),
 
-CONSTRAINT cp_waller PRIMARY KEY(idWallet),
-CONSTRAINT ca_usuario FOREIGN KEY(idUsuario) REFERENCES Usuario(id) ON DELETE RESTRICT ON UPDATE CASCADE
+CONSTRAINT cp_wallet PRIMARY KEY(idWallet),
+CONSTRAINT ca_users FOREIGN KEY(idUser) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE CASCADE
 
 );
 
@@ -27,10 +25,10 @@ CREATE TABLE Offers(
     walletSeller    VARCHAR(40),
     quantity        INTEGER,
     dateOffer       DATE NOT NULL,
-    idUsuario       VARCHAR(9),
+    idUser          VARCHAR(9),
 
 CONSTRAINT cp_offer PRIMARY KEY(walletSeller),
-CONSTRAINT ca_usuario FOREIGN KEY(idUsuario) REFERENCES Usuario(id) ON DELETE RESTRICT ON UPDATE CASCADE
+CONSTRAINT ca_users FOREIGN KEY(idUser) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE CASCADE
 
 );
 
@@ -39,11 +37,11 @@ CREATE TABLE Transactions(
     walletBuyer     VARCHAR(40) NOT NULL,
     walletSeller    VARCHAR (40) NOT NULL,
     quantity        INTEGER NOT NULL,
-    trans_date      Date
+    trans_date      Date,
 
 CONSTRAINT cp_Transactions PRIMARY KEY (walletBuyer, walletSeller)
 );
 
 
 
-INSERT INTO "User" VALUES('12345678N',0,123456789,18,'admin@admin.com','Admin','/rPjddRcrpc7IPtD8oN+QoD/8KrIM7CD',623457698);
+INSERT INTO Users VALUES('12345678N',0,123456789,18,'admin@admin.com','Admin','/rPjddRcrpc7IPtD8oN+QoD/8KrIM7CD',623457698);

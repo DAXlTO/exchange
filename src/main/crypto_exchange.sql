a6480d9655659527e2b645ed46bc023f389d40e2
carlos-ip <93195318+carlos-ip@users.noreply.github.com>
2021-12-07 13:34:03 +0100

    Add files via upload

---
+++ src/main/crypto_exchange.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO users VALUES('44712348A',200,546541589,42,'pepe@gmail.com','Pepe','pepepas', 647589654);
+INSERT INTO users VALUES('41026985B',0,546995478,30,'marta@gmail.com','Marta','martapass',647589615);
+INSERT INTO users VALUES('44568748C',1000,546541589,42,'alex@gmail.com','Alex','alexpass',647589620);
+
+INSERT INTO offers VALUES(0,0.2,'30/11/2021','44568748C');
+INSERT INTO offers VALUES(1,0.05,'29/11/2021','44568748C');
+INSERT INTO offers VALUES(2,0.05,'9/11/2021','44568748C');
+INSERT INTO offers VALUES(3,0.005,'10/11/2021','41026985B');
+INSERT INTO offers VALUES(4,0.0002,'28/11/2021','41026985B');
+INSERT INTO offers VALUES(5,0.01,'14/11/2021','44712348A');
+
+INSERT INTO wallet VALUES('mkyr1a0411h6ys2it6pumxj86ap13z2lx24gkc7q',2,'44568748C');
+INSERT INTO wallet VALUES('n4w6x37gh3zurzltpkyq9urfhkftzhm3ktk3dsg0',0.015,'41026985B');
+INSERT INTO wallet VALUES('gnayhboldb4xfa5977glsics9bupgj7eperc9zxy',0.03,'44712348A');
+
+
+INSERT INTO transactions VALUES(0,'mkyr1a0411h6ys2it6pumxj86ap13z2lx24gkc7q','n4w6x37gh3zurzltpkyq9urfhkftzhm3ktk3dsg0',0.2,'07/12/2021');
+INSERT INTO transactions VALUES(1,'mkyr1a0411h6ys2it6pumxj86ap13z2lx24gkc7q','n4w6x37gh3zurzltpkyq9urfhkftzhm3ktk3dsg0',0.001,'30/11/2021');
+INSERT INTO transactions VALUES(25,'gnayhboldb4xfa5977glsics9bupgj7eperc9zxy','mkyr1a0411h6ys2it6pumxj86ap13z2lx24gkc7q',0.05,'05/12/2021');
+
